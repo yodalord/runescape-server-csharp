d09a39f509d6871fbe76ab37224038cb04fa4693
pkedpker@gmail.com <pkedpker@gmail.com@fd3b4ddb-cdad-e9e7-605d-ca100c99e9ed>
2013-10-07 18:14:58 +0000

    Fixed MapData problem. Had 668 MapData needed 530 MapData.

---
+++ trunk/ --username pkedpker@gmail.com/runescape-server-csharp/Runescape 530 Server/packethandler/PacketHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using RunescapeServer.player;
+using RunescapeServer.net;
+
+namespace RunescapeServer.packethandler
+{
+    interface PacketHandler
+    {
+        void handlePacket(Player player, Packet p);
+    }
+}
